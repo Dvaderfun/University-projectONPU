fa002de8e62fa5d92b8571843e6dc6875630944d
ArtyomKazlanzhi <artem20_11@icloud.com>
2019-04-11 01:06:57 +0300

    реализовал большинство методов

--- src/Group.java
+++ src/Group.java
@@ -1,2 +1,85 @@
+import java.lang.reflect.Array;
+
 public class Group {
+
+    private int groupId;
+    private Student[] students;
+
+
+    public Group(int groupId) {
+        this.groupId = groupId;
+        this.students = new Student[0];
+    }
+
+    public Group(int groupId, int numberOfStud) {
+        this.groupId = groupId;
+        this.students = new Student[numberOfStud];
+    }
+
+    public Group(Student[] students) {
+        this.students = students;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public int getNumOfStudents() {
+        return students.length;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Student findStudent(int studentId) {
+        for (Student s : students) {
+            if (s.getStudentId() == studentId) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    private int findStudentIndex(int studentId) {
+
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].getStudentId() == studentId) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+
+    public boolean removeStudent(Student[] students, int studentId) {
+        int studentIndex = findStudentIndex(studentId);
+
+        if (studentIndex != 0) {
+            System.arraycopy(students, studentIndex + 1, students, studentIndex,
+                    students.length - 1 - studentIndex);
+            this.students = students;
+            return true;
+        }
+        return false;
+    }
+
+    public void addStudent(Student[] students, Student newStudent) {
+
+        Student[] newArray = (Student[]) Array.newInstance(students.getClass().getComponentType(),
+                            students.length + 1);
+        System.arraycopy(students, 0, newArray, 0, students.length);
+        System.arraycopy(new Student[] {newStudent}, 0, newArray, students.length, 1);
+        this.students = newArray;
+    }
+
+
 }
+
+
+
+
