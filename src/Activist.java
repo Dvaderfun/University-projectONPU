deecd74ec2ac8caffed955f156028b9151446217
vovBorya <vovobr02@gmail.com>
2019-04-11 18:39:16 +0300

    Activist: DONE

--- src/Activist.java
+++ src/Activist.java
@@ -1,6 +1,7 @@
 public interface Activist {
+
     //возвращающий количество мероприятий, в которых участвовал студент;
-    int getEventNumber();
+    int getEventNubmer();
 
     //возвращающий число призовых мест, занятых на олимпиадах;
     int getPrizePlaceNumber();
@@ -10,5 +11,5 @@ public interface Activist {
 
     //строка состоит из названий проектов,
     //за которые студент получил вознаграждение на соревнованиях.
-     String getNameProjects();
+    String getNameProjects();
 }
