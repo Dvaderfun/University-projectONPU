b63f0174f3f379c6cd0c389b3c221954c351098c
titarenko <denis.titarenko2@gmail.com>
2019-04-11 20:00:51 +0300

    Conference

---
+++ src/Conference.java
@@ -0,0 +1,36 @@
+import java.util.Date;
+
+public class Conference implements Event {
+    private Date date;
+    private String city;
+    private String articleName;
+
+
+    public String getArticleName() {
+        return articleName;
+    }
+
+    public void setArticleName(String articleName) {
+        this.articleName = articleName;
+    }
+
+    @Override
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
