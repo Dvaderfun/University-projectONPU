d5caf06023b50dbe05d9c92879e229b79aaa9c6d
titarenko <denis.titarenko2@gmail.com>
2019-04-12 15:53:50 +0300

    Тесты к классу Olympiad

--- test/OlympiadTest.java
+++ test/OlympiadTest.java
@@ -1,3 +1,5 @@
+import exceptions.IncorrectDataException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,11 +12,14 @@ class OlympiadTest {
     }
 
     @Test
+    @DisplayName("Указать город!")
     void setCity() {
-        assertThrows(Olympiad.IncorrectNameCity.class, () -> olympiad.setCity(""));
+        assertThrows(IncorrectDataException.class, () -> olympiad.setCity("  "));
     }
 
     @Test
+    @DisplayName("Несуществующее место на олимпиаде!")
     void setPodiumPlace() {
+        assertThrows(IncorrectDataException.class, () -> olympiad.setPodiumPlace(0));
     }
 }
