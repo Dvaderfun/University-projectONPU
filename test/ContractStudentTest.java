a4ed0537a3b03760e8a397c66c78a0910df2cfe2
vovBorya <vovobr02@gmail.com>
2019-04-16 19:49:16 +0300

    /

---
+++ test/ContractStudentTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContractStudentTest {
+
+    @Test
+    void getPaymentsList() {
+    }
+
+    @Test
+    void setPaymentsList() {
+    }
+
+    @Test
+    void getCostEducationSemestr() {
+    }
+
+    @Test
+    void setCostEducationSemestr() {
+    }
+
+    @Test
+    void getAmountDebt() {
+    }
+
+    @Test
+    void addPayment() {
+    }
+
+    @Test
+    void deletePayment() {
+    }
+
+    @Test
+    void toString() {
+    }
+}
