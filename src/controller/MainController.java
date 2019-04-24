f01795d94ddb07cc90a281b896db7f040a95508f
Daniil Dermenzhy <dvaderfun@gmail.com>
2019-04-25 01:02:55 +0300

    UI making

---
+++ src/controller/MainController.java
@@ -0,0 +1,74 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+import model.Group;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    private int i = 1;
+
+    @FXML
+    private Button addGroup;
+
+    @FXML
+    private Button removeGroup;
+
+    @FXML
+    private Button openGroup;
+
+    @FXML
+    private Text groupQuantity;
+
+    @FXML
+    private ListView listView;
+
+    private Main main;
+
+    public void setMain(Main main) {
+        this.main = main;
+        main.bindGroups(listView);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        addGroup.setOnAction(event -> {
+            Group group = new Group(i, 0);
+            main.addGroup(group);
+            i++;
+
+            groupQuantity.setText(getGroupQuantity());
+        });
+
+        removeGroup.setOnAction(event -> {
+            main.deleteGroup((Group) listView.getSelectionModel().getSelectedItem());
+            groupQuantity.setText(getGroupQuantity());
+                    });
+
+        openGroup.setOnAction(event -> {
+            Group group = (Group) listView.getSelectionModel().getSelectedItem();
+            main.openGroup(group.getGroupId());
+        });
+
+
+        listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (listView.getSelectionModel().getSelectedItem() != null) {
+                removeGroup.setDisable(false);
+                openGroup.setDisable(false);
+            } else {
+                removeGroup.setDisable(true);
+                openGroup.setDisable(true);
+            }
+            groupQuantity.setText(getGroupQuantity());
+        });
+    }
+
+    private String getGroupQuantity() {
+        return "Groups: " + listView.getItems().size();
+    }
+}
--- src/model/ModelFacade.java
+++ src/model/ModelFacade.java
@@ -12,20 +12,57 @@ public class ModelFacade extends Observable {
     // Фасад (Facade)
 
     private ObservableList<Group> list;
+    private ObservableList<Student> studentsList;
+
+    Group group;
 
     public ModelFacade() {
         list = FXCollections.observableArrayList();
+        studentsList = FXCollections.observableArrayList();
     }
 
     public void addGroup(Group e) {
         list.add(e);
     }
 
+    public Group getGroup(int groupId) {
+        return list.get(groupId);
+    }
+
     public void deleteGroup(Group e) {
         list.remove(e);
     }
 
-    public void bindObserver(ListView listView) {
+    public void bindGroups(ListView listView) {
         listView.setItems(list);
+    }
+
+    public void updateStudentsList(int groupId){
+        group = getGroup(groupId);
+
+        studentsList.clear();
+        studentsList.addAll(group.getStudents());
+    }
+
+    public void addStudent(Student s, int groupId) {
+        group = getGroup(groupId);
+        group.addStudent(s);
+
+        list.set(groupId, group);
+
+        studentsList.add(s);
+    }
+
+    public void deleteStudent(Student s, int groupId) {
+        group = getGroup(groupId);
+        group.removeStudent(s.getStudentId());
+
+        list.set(groupId, group);
+
+        studentsList.remove(s);
+    }
+
+    public void bindStudents(ListView listView) {
+        listView.setItems(studentsList);
     }
 }
